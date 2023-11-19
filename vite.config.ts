d45bf4ad93d7e1ba2915b7303343c33aac5dd644
David Flanik <Daveflanik@gmail.com>
2023-11-19 14:35:45 +0900

    configured, set up and tested vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+/// <reference types="@testing-library/jest-dom" />
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,5 +10,10 @@ export default defineConfig({
     plugins: [react()],
     server: {
         port: 3000,
+    },
+    test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: ['./src/setupTests.ts'],
     },
 });
