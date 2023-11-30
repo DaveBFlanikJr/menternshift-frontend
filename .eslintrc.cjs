e2d97b483dea0194b4d25e3709727a4b579e6506
DaveBFlanikJr <74096717+DaveBFlanikJr@users.noreply.github.com>
2023-11-30 21:04:14 +0900

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,17 @@
 module.exports = {
+    root: true,
+    env: { browser: true, es2020: true },
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'prettier'],
+    ignorePatterns: ['dist', '.eslintrc.cjs'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['react-refresh', 'react'],
+    rules: {
+        'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+    settings: {
+        react: {
+            version: 'detect',
+        },
   root: true,
   env: { browser: true, es2020: true },
   extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:react-hooks/recommended", "prettier"],
